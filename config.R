356f7c678be014966e48cea09e75a9f1bcf26497
jbryer <jason@bryer.org>
2023-07-14 16:48:10 -0400

    Updated site configuration

--- config.R
+++ config.R
@@ -1,6 +1,11 @@
+year <- 2023
+semester <- 'Fall'
 github_link <- "DATA606Fall2023"
 one_minute_paper <- 'https://forms.gle/ngYXfC6jwY3TV6FXA'
 one_minute_paper_results <- "https://docs.google.com/spreadsheets/d/1FFs2vR7GUR4XwqxaOENp6w3_5N8kMWzuGbZR_kLYnUg/edit?resourcekey#gid=1125424984"
+formative_assessment <- 'https://forms.gle/6YDZEkq3uhKanJPs9'
+formative_assessmnet_results <- 'https://docs.google.com/spreadsheets/d/189ZoZgrK8iqsApRv67IdFAj866yiNblnbtGkwVIXVow/edit?resourcekey#gid=1436273879'
+slack_invite_link <- ''
 
 # remotes::install_github("gadenbuie/countdown")
 # devtools::install_github("ropenscilabs/icon")
@@ -23,7 +28,11 @@ library("psych")
 library("cowplot")
 library("rmarkdown")
 
-source('word_cloud.R')
+# source('word_cloud.R')
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
 
 opts_chunk$set(digits = 3, width = 120)
 
@@ -95,3 +104,31 @@ PlotDist <- function(alpha, from = -5, to = 5, n = 1000, filename = NULL,
 	if (!is.null(filename)) dev.off()
 }
 
+ompWordCloud <- function(text) {
+	if(length(text) < 1) {
+		warning('No text to create a word cloud.')
+		return()
+	}
+	docs <- Corpus(VectorSource(text))
+	# Convert the text to lower case
+	docs <- tm_map(docs, content_transformer(tolower))
+	# Remove numbers
+	docs <- tm_map(docs, removeNumbers)
+	# Remove english common stopwords
+	docs <- tm_map(docs, removeWords, stopwords("english"))
+	# Remove your own stop word
+	# specify your stopwords as a character vector
+	docs <- tm_map(docs, removeWords, c("blabla1", "blabla2"))
+	# Remove punctuations
+	docs <- tm_map(docs, removePunctuation)
+	# Eliminate extra white spaces
+	docs <- tm_map(docs, stripWhitespace)
+	dtm <- TermDocumentMatrix(docs)
+	m <- as.matrix(dtm)
+	v <- sort(rowSums(m),decreasing=TRUE)
+	d <- data.frame(word = names(v),freq=v)
+	head(d, 10)
+	wordcloud(words = d$word, freq = d$freq, min.freq = 2,
+			  max.words=200, random.order=FALSE, rot.per=0.35,
+			  colors=brewer.pal(8, "Dark2"))
+}
